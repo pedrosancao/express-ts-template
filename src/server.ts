da8aa50f03ae5cc1bd7cf22cbd91eb96e9c06485
Pedro Sanção <pedrosancao@users.noreply.github.com>
2020-09-05 14:52:02 -0300

    Add JSON body parser

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, { json } from 'express';
 import routes from './routes';
 
 const app = express();
 const port = 3333;
 
+app.use(json());
 app.use(routes);
 
 app.listen(port, () => {
