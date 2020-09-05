917abe66ee94a0bd8872efd1e9c16f74d0ac8ee8
Pedro Sanção <pedrosancao@users.noreply.github.com>
2020-09-05 14:30:47 -0300

    Create template

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+const port = 3333;
+
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`); // eslint-disable-line no-console
+});
